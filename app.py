672ebc922a73b97390104c333fd2d50ecfe8b550
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-02-23 18:19:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS  # Import the CORS package
 import openai
-
+import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
