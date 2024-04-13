6676634572a73f738990aca5d8d1ac7799a3ffb8
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-04-12 18:06:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,8 @@ def respondToQuery(query):
   completion = client.chat.completions.create(
       model="gpt-3.5-turbo-0125",
       messages=[{
-          "role":
-          "system",
-          "content":
-          "You are a helpful assistant designed to output JSON.",
-      }, {
           "role": "user",
-          "content": sentence
+          "content": query
       }])
 
   message = completions.choices[0].text
