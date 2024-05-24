e69342a06ab727e568cd9b33e994bc6b488187d3
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-05-23 22:55:20 -0700

    API v2 update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS  # Import the CORS package
-import openai
+from openai import OpenAI
 import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # OpenAI API key
 OpenaiApiKey = os.environ.get('OpenaiApiKey')
-client = openai.Client(api_key=OpenaiApiKey)
+client = OpenAI(api_key=OpenaiApiKey)
 
 @app.route("/query/<query>")
 def respondToQuery(query):
@@ -15,7 +15,7 @@ def respondToQuery(query):
   prompt = (f"{query}")
 
   completion = client.chat.completions.create(
-      model="gpt-3.5-turbo-0125",
+      model="gpt-4-o",
       messages=[{
           "role": "user",
           "content": query
